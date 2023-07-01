6568a26ab7a078f0906142199d4f30701f2ead8c
Dong Hyeon Oh <dev0doraemon@gmail.com>
2023-07-01 15:59:56 +0900

    변경사항 - 과거 깃허브에서 merge 작업에 문제가 있었는지 코드가 다시 실행 안되는   문제가 발생하여 전체적으로 수정 - prettierrc 파일 추가 - 인증된 사용자이지만 권한이 없는 작업을 수행할 시 로그아웃 되는 버그   존재

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -1,33 +1,39 @@
-const jwt = require("jsonwebtoken");
-const { Users } = require("../models");
-const tokenKey = "4joTuna";
+const jwt = require('jsonwebtoken');
+const { Users } = require('../models');
+const tokenKey = '4joTuna';
 
 const authMiddleware = async (userFieldNamesArray, req, res, next) => {
-  try {
-    const { Authorization } = req.cookies;
-    const [authType, authToken] = (Authorization ?? "").split(" ");
-    if (!authToken || authType !== "Bearer") {
-      return res.status(403).json({ message: "로그인이 필요한 기능입니다" });
+    try {
+        const { Authorization } = req.cookies;
+        const [authType, authToken] = (Authorization ?? '').split(' ');
+
+        if (!authToken || authType !== 'Bearer') {
+            return res
+                .status(403)
+                .json({ message: '로그인이 필요한 기능입니다' });
+        }
+        const { email } = jwt.verify(authToken, tokenKey);
+        const user = await Users.findOne({
+            where: { email },
+            attributes: userFieldNamesArray.concat('refreshToken'), //배열안에 REFRESH_TOKEN과 함께, 가져오고싶은 필드이름을 지정 (push는 길이반환 , concat은 배열반환)
+        });
+
+        if (!user) {
+            // res.clearCookie('Authorization');
+            // 쿠키를 지우기 때문에 로그인 한 유저가 권한이 없는 작업을 수행했을 경우 로그아웃이 되는 문제 발생
+            return res
+                .status(403)
+                .json({ message: '로그인이 필요한 기능입니다' });
+        }
+        //리프레시토큰으로 인증기간이 맞는지 확인
+        jwt.verify(user.refreshToken, tokenKey);
+        res.locals.user = user;
+        next();
+    } catch (err) {
+        console.log(err);
+        res.clearCookie('Authorization');
+        return res.status(403).json({ message: '로그인에 실패하였습니다' });
     }
-    const { email } = jwt.verify(authToken, tokenKey);
-    const user = await Users.findOne({
-      where: { email },
-      attributes: userFieldNamesArray.concat("refreshToken"), //배열안에 REFRESH_TOKEN과 함께, 가져오고싶은 필드이름을 지정 (push는 길이반환 , concat은 배열반환)
-    }); //
-    
-    if (!user) {
-      res.clearCookie("Authorization");
-      return res.status(403).json({ message: "로그인이 필요한 기능입니다" });
-    }
-    //리프레시토큰으로 인증기간이 맞는지 확인
-    jwt.verify(user.refreshToken, tokenKey);
-    res.locals.user = user;
-    next();
-  } catch (err) {
-    console.log(err)
-    res.clearCookie("Authorization");
-    return res.status(403).json({ message: "로그인에 실패하였습니다" });
-  }
 };
 
 module.exports = authMiddleware;
